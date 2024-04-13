4d0128a7c940d0f6af065f06f5a4ec495505c0e0
Gokulsankar Thangaraj <113358646+Gokulsankar-21@users.noreply.github.com>
2024-04-13 14:06:42 +0000

    complete the portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const projects = [
   {
     id: 1,
-    color: "from-red-300 to-blue-300",
+    color: "from-red-100 to-blue-100",
     title: "Mern Blog App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quisquam quis, iste dignissimos doloribus voluptatem aspernatur ",
     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
@@ -15,23 +15,23 @@ const projects = [
   },
   {
     id: 2,
-    color: "from-blue-300 to-violet-300",
-    title: "Book Store",
+    color: "from-blue-100 to-violet-100",
+    title: "Book Store App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quisquam quis, iste dignissimos doloribus voluptatem aspernatur ",
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     link: "https://gokuldev.in",
   },
   {
     id: 3,
-    color: "from-violet-300 to-purple-300",
-    title: "IMDB Clone",
+    color: "from-violet-100 to-purple-100",
+    title: "IMDB Clone App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quisquam quis, iste dignissimos doloribus voluptatem aspernatur ",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     link: "https://gokuldev.in",
   },
   {
     id: 4,
-    color: "from-purple-300 to-red-300",
+    color: "from-purple-100 to-red-100",
     title: "Socio Pedia App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quisquam quis, iste dignissimos doloribus voluptatem aspernatur ",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -65,7 +65,7 @@ export default function Portfolio() {
 
           {/* Intha container than move panna poren */}
           <motion.div className="flex " style={{ x }}>
-            <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+            <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-purple-100 to-red-100" />
 
             {/* PROJECT LIST CONTAINER*/}
             {projects.map((project) => (
@@ -77,19 +77,27 @@ export default function Portfolio() {
                 {/* PROJECT INFO */}
                 <div className="flex flex-col gap-8 text-white">
                   {/* PROJECT TITLE */}
-                  <h1>{project.title}</h1>
+                  <h1 className="text-3xl font-bold  md:text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-gray-700">
+                    {project.title}
+                  </h1>
 
                   {/* PROJECT IMAGE */}
-                  <div className="relative">
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[400px] lg:h-[300px] xl:w-[500px] xl:h-[350px] 2xl:w-[600px] 2xl:h-[420px]">
                     <Image src={project.img} alt={project.title} fill />
                     {/* Next js la imaeg intha maari optimize pannalam */}
                   </div>
 
                   {/* PROJECT DESC */}
-                  <p>{project.desc}</p>
-                  <Link href={project.link}>
-                    <button>See Dome</button>
-                  </Link>
+                  <p className="lg:text-xl w-80 md:w-96 lg:w-[400px] xl:w-[500px] 2xl:w-[600px] text-slate-700 font-normal">
+                    {project.desc}
+                  </p>
+                  <div className=" flex justify-end lg:translate-x-1/3">
+                    <Link href={project.link}>
+                      <button className="p-2 text-sm  font-semibold text-slate-600 md:p-4 md:text-lg lg:p-6 xl:p-8 rounded bg-white hover:scale-110 transition duration-300 hover:shadow-sm ">
+                        See Demo
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
             ))}
