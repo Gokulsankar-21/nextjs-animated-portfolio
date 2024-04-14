2ec998cb5127d0d953a27bbbabc9030704be3818
Gokulsankar Thangaraj <113358646+Gokulsankar-21@users.noreply.github.com>
2024-04-14 03:17:48 +0000

    complete the email functionality using emailjs package

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,36 @@
 "use client";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { IoSendSharp } from "react-icons/io5";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
   const text = "Say  Hello";
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    setError(false);
+    setSuccess(false);
+    e.preventDefault();
+    if (!formRef.current[0].value || !formRef.current[1].value) {
+      setError("Please fill required fields ...!");
+      return;
+    }
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        formRef.current,
+        { publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY }
+      )
+      .then(() => {
+        setSuccess(true);
+      })
+      .catch((error) => setError(error.text));
+  };
   return (
     <motion.div
       initial={{ y: "-200vh" }}
@@ -28,21 +54,37 @@ export default function Contact() {
           <span className="ml-2"> 😊</span>
         </div>
         {/* FORM CONTAINER  */}
-        <form className="h-full w-full lg:h-full lg:w-1/2 bg-red-50 flex flex-col gap-8 justify-center p-12 lg:p-24 rounded-lg">
+        <form
+          onSubmit={sendEmail}
+          ref={formRef}
+          className="h-full w-full lg:h-full lg:w-1/2 bg-red-50 flex flex-col gap-8 justify-center p-12 lg:p-24 rounded-lg"
+        >
           <span className="text-xl ">Dear Gokul Dev,</span>
           <textarea
             rows={5}
             className="bg-transparent  border-b-2 border-b-black resize-none outline-none"
+            name="user_message"
           />
           <span className="text-xl ">My mail address is:</span>
           <input
             type="text"
             className="bg-transparent border-b-2 border-b-black outline-none"
+            name="user_email"
           />
           <span className="text-xl ">Regards</span>
           <button className="p-4 font-semibold bg-purple-300 text-gray-600 rounded-lg hover:bg-purple-400 hover:text-white hover:scale-105 transition duration-300 flex gap-4 items-center justify-center text-lg md:text-xl">
-            Send <IoSendSharp className="text-xl"/>
+            Send <IoSendSharp className="text-xl" />
           </button>
+          {success && (
+            <span className="font-semibold text-green-600">
+              Email Send Successfully !
+            </span>
+          )}
+          {error && (
+            <span className="font-semibold text-red-600">
+              {error || "Something went wrong. Please Try Again !"}
+            </span>
+          )}
         </form>
       </div>
     </motion.div>
@@ -51,4 +93,20 @@ export default function Contact() {
 /**
  *
  * inga staggered animation aha nama Pure JS method la pandra maari 1.1 2.1 delay vechi animate pannitom
+ */
+
+/**
+ * Next la nama env variables ah NEXT_PUBLIC keyword use panni add pannuvom
+ *
+ * EmailJS - Email as a service
+ *  so nama manaual ah backend la irunthu email ah namaku anupuvom so nama backend ilama intha services ah use pannikalam
+ * athula Service - nama entha services ah choose pandrom  -Gmail or Outlook
+ * athula public key for authentication
+ * template id - athu email oda template
+ * nama kudutha detailes ah vechi nama account la irukura service id and template id ah vechi athu namaku email aha send pannum
+ */
+/**
+ * to me
+ * ennoda emailjs account la gokulsankar2002t@gmail.com kuduthu iruken
+ * athula yaru email anupuranagaloa athu vaangi template la vechi anga kudutha email (gokulsankar2002t@gmail.com) ennoda email (gokulsankar2002t@gmail.com) la irunthu ennoda email kae varum
  */
